4cee2b6edefaac86b2c9068137582a90fdb15e9a
nickel-dime <goepnik@gmail.com>
2022-12-27 13:27:37 -0500

    random test

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            Hi&nbsp;
+            <code className={styles.code}>Nikhita!</code>
           </p>
           <div>
             <a
