f37345e4f83429aa44b749b76cdc8eeeeb24d63a
nickel-dime <goepnik@gmail.com>
2023-08-13 23:57:37 -0400

    work?

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,45 @@ import "../styles/globals.css";
 
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  if (router.pathname == "/studio") {
+    return (
+      <>
+        <svg className="hidden">
+          <filter id="noiseFilter">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.7"
+              stitchTiles="stitch"
+            />
+            <feColorMatrix
+              in="colorNoise"
+              type="matrix"
+              values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 1 0"
+            />
+            <feComposite operator="in" in2="SourceGraphic" result="monoNoise" />
+            <feBlend in="SourceGraphic" in2="monoNoise" mode="screen" />
+          </filter>
+        </svg>
+
+        <div className="testStyle font-sans">
+          {/* <div className="fixed inset-0 flex justify-center sm:px-8">
+          <div className="flex w-full max-w-7xl lg:px-8">
+            <div className="w-full" />
+          </div>
+        </div> */}
+          <div className="module-inside flex flex-col font-fira min-h-screen text-slate-900 bg-[#fcedc7]">
+            <main className="grow">
+              <Component {...pageProps} />
+            </main>
+          </div>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <svg className="hidden">
@@ -30,15 +67,11 @@ export default function App({ Component, pageProps }) {
           </div>
         </div> */}
         <div className="module-inside flex flex-col font-fira min-h-screen text-slate-900 bg-[#fcedc7]">
-          <div className="">
-            <Header></Header>
-          </div>
+          <Header></Header>
           <main className="grow">
             <Component {...pageProps} />
           </main>
-          <div className="">
-            <Footer></Footer>
-          </div>
+          <Footer></Footer>
         </div>
       </div>
     </>
