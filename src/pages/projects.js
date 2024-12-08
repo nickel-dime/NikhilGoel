b87c2f7a0e05f05d66836ac04fcdaf0698e28de0
shanthansharanya <sharanya.shanthan@gmail.com>
2024-12-08 11:28:17 -0500

    Better name

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -28,7 +28,7 @@ export default function Projects({ prevURL = null, projects }) {
   return (
     <>
       <Head>
-        <title>Sharanya Shanthan</title>
+        <title>Nesquick Goel</title>
         <meta name="description" content="Personal website for Nikhil Goel" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/ndimelogo.png" />
